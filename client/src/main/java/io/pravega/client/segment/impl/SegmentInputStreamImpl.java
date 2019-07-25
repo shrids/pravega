a5179285b1acbd3d70c67500bc3431e53f30640f
Sandeep <sandeep.shridhar@emc.com>
2019-07-25 00:25:24 -0400

    Change cancel read to info level logging.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
@@ -196,9 +196,9 @@ class SegmentInputStreamImpl implements SegmentInputStream {
     public void close() {
         log.trace("Closing {}", this);
         if (outstandingRequest != null) {
-            log.debug("Cancel outstanding read request for segment {}", asyncInput.getSegmentId());
+            log.info("Cancel outstanding read request for segment {}", asyncInput.getSegmentId());
             outstandingRequest.cancel(true);
-            log.debug("Completed cancelling outstanding read request for segment {}", asyncInput.getSegmentId());
+            log.info("Completed cancelling outstanding read request for segment {}", asyncInput.getSegmentId());
         }
         asyncInput.close();
     }
